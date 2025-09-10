fbed66d27c3d61ada142603716c4ae1302f67954
Al2jan7ro <xxzanderplaysxx27@gmail.com>
2025-09-10 14:12:37 -0400

    SEO & Proyectos con imagen oscurecida

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-// @ts-check
+
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
+
 export default defineConfig({
+
+  site: "https://al2jan7ro.vercel.app",
+  integrations: [sitemap()],
 
 server: {
   host: true
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,19 +1,46 @@
+// src/data/projects.ts
 import Psico from "../components/work/SystemPsico.astro";
 import Reports from "../components/work/SystemReports.astro";
 import Landing from "../components/work/LandingAndes.astro";
 import Blog from "../components/work/LaCalifornia.astro";
 
-export const projects: Record<string, any> = {
-  psico: Psico,
-  reports: Reports,
-  landing: Landing,
-  blog: Blog,
+export interface ProjectData {
+  component: any; // el componente Astro
+  title: string;
+  description: string;
+  image?: string;
+}
+
+export const projects: Record<string, ProjectData> = {
+  psico: { 
+    component: Psico, 
+    title: "Sistema de Psicología", 
+    description: "Sistema web para agendar y gestionar citas psicológicas con reportes y videollamadas.",
+    image: "/assets/Psico.webp" },
+
+  reports: { 
+    component: Reports, 
+    title: "Sistema Reportes", 
+    description: "Plataforma de gestión de reportes migrada a Astro con backend en NestJS.",
+    image: "/assets/Reportes.webp" },
+
+  landing: { 
+    component: Landing, 
+    title: "Landing Lácteos", 
+    description: "Landing page para el departamento de sistemas de lácteos los andes.",
+    image: "/assets/Landing.webp" },
+
+  blog: { 
+    component: Blog, 
+    title: "Blog La California", 
+    description: "Blog informativo con diseño personalizado para una comunidad local.",
+    image: "/assets/Blog.webp" },
 };
 
-// Mapeo para declrar las rutas
+// Mapeo de rutas dinámicas
 export function getProjectPaths() {
   return Object.keys(projects).map((id) => ({
     params: { id },
-    props: { id },//id como props para enviar a la pagina
+    props: { id },
   }));
 }
